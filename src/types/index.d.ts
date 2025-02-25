1575eaa2f6e2cf21383cbdb93942de2e39fef10a
mcarina <marciacarina931@gmail.com>
2025-02-25 10:48:22 -0400

    declare project in index.d

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -2,3 +2,15 @@ declare interface FilterProps {
     filter: FilterType;
     setFilter: (filter: FilterType) => void;
 }
+
+declare interface Project {
+    id: string;
+    name: string;
+    status: "completed" | "in-progress" | "planned";
+    longDescription: string;
+    tech?: string[];
+    features?: string[];
+    challenges?: string[];
+    demoUrl: string;
+    githubUrl: string;
+}
