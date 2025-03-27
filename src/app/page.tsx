a8581276071205b59e031430a2d9dff560bfffd1
mcarina <marciacarina931@gmail.com>
2025-03-27 15:31:05 -0400

    home na versao mobile, ainda nao finalizado

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,19 +5,20 @@ import { Sparkles } from "lucide-react"
 
 export default function Home() {
   return (
-    <main className="sm:min-h-screen p-0">
-      <div className="home" >
+      <div className="home min-h-screen" >
         <div className="home-container">
           
           <pre className='flex flex-cols h-full flex-col items-center justify-center space-y-2.5 whitespace-pre md:space-y-3 w-full'>
             <div className="flex justify-center">
               <Sparkles className="w-8 h-8 text-primary animate-pulse" />
             </div>
-            <code className='text-[2dvw] leading-[0.9] tracking-tight lg:text-[1.4dvh] select-none'>{ascii}</code>
-            <code className='text-center text-sm md:text-base'>
+            <code className='text-[2dvw] leading-[0.9] tracking-tight lg:text-[1.4dvh] select-none '>{ascii}</code>
+            <div className='text-center text-sm md:text-base'>
               <h2>Full Stack web developer</h2>
-              <p className="p-subtext text-muted-foreground leading-relaxed ">Aprendendo e resolvendo novos problemas de codigo</p>
-            </code>
+              <p className="p-subtext text-muted-foreground leading-relaxed mt-2 text-balance max-w-[220px] mx-auto sm:max-w-none">
+                Aprendendo e resolvendo novos problemas de código
+              </p>
+            </div>
               
               <CardHome/>
               <SobreProjeto/>
@@ -28,9 +29,6 @@ export default function Home() {
 
 
       </div>
-    </main>
-
-
 );
 }
 
--- src/components/NavMobile.tsx
+++ src/components/NavMobile.tsx
@@ -1,33 +1,46 @@
 'use client'
 import React from 'react'
 import Link from 'next/link'
-import { useSelectedLayoutSegment } from 'next/navigation'
 import { menu } from '@/lib/constants'
+import { useSelectedLayoutSegment } from 'next/navigation'
+import { AlignJustify } from 'lucide-react'
+import { Sheet, SheetContent, SheetTrigger } from './ui/sheet'
 
 export const MobileNav = () => {
 
     const segment = useSelectedLayoutSegment()
 
   return (
-    <div className="flex w-full flex-col bg-muted/40">
-        <div className="sm:hidden flex flex-col  sm:gap-4 sm:py-4 sm:pl-14">
-                <ul className='flex items-center'>
-                    {menu.map(item => {
-                        const isActive = segment === item.href.split('/')[1] || (segment === null && item.href === '/')
-                        return(
-                            <li key={item.title}>
-                                <Link 
-                                    
-                                    href={item.href}
-                                    className={`navLink ${isActive ? 'bg-[#adadada6] text-black' : 'text-[#BDBDBDE6]'}`}
-                                >
-                                    <span className="text-primary">&gt;</span> ~/
-                                    {item.title}
-                                </Link>
-                            </li>
-                        )
-                    })}
-                </ul>
+    <div className="flex w-full flex-col bg-muted/40 p-4">
+        <div className="sm:hidden flex flex-col sm:gap-4 sm:py-4 sm:pl-14">
+            <Sheet>
+                <SheetTrigger asChild >
+                    <button className="sm:hidden">
+                        <AlignJustify/>
+                        <span className="sr-only">abrir / fechar</span>
+                    </button>
+                </SheetTrigger>
+                
+                <SheetContent side="left" className="w-full sm:max-w-x">
+                        <ul className='flex flex-col items-center p-2 gap-5'>
+                            {menu.map(item => {
+                                const isActive = segment === item.href.split('/')[1] || (segment === null && item.href === '/')
+                                return(
+                                    <li key={item.title}>
+                                        <Link 
+                                            
+                                            href={item.href}
+                                            className={`p-2 ${isActive ? 'bg-[#adadada6] text-[#ffff]' : 'text-[#ffff]'}`}
+                                        >
+                                            <span className="text-primary">&gt;</span> ~/
+                                            {item.title}
+                                        </Link>
+                                    </li>
+                                )
+                            })}
+                        </ul>
+                </SheetContent>
+            </Sheet>
         </div>
     </div>
   )
